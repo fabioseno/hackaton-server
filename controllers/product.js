0feaeae96ac58faf5fd14ba7e34fb002cc947b82
Fábio Akira Yoshida <fabioseno@gmail.com>
2016-08-11 19:27:34 -0300

    Mocking product

--- controllers/product.js
+++ controllers/product.js
@@ -1,182 +1,156 @@
-/*global module, require, Buffer*/
-var http = require('https'),
-    querystring = require('querystring'),
-
-    host = 'vendadigitalmb.lojasrenner.com.br',
-    appKey = 'c81efd66a79b6c5a58bfad182f150695',
-    appSecret = '13c3374b5d3332c4b2efef4d04d47c26',
-
-    cookies,
-
-    connectToMobileFabric = function (success) {
-        'use strict';
-
-        var options = {
-            host: host,
-            path: '/authService/100000002/login',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'X-Kony-App-Secret': appSecret,
-                'X-Kony-App-Key': appKey,
-                'Accept': 'application/json'
-            }
-        },
-
-            callback = function (response) {
-                var str = '', setcookie;
-
-                cookies = [];
-
-                setcookie = response.headers["set-cookie"];
-
-                if (setcookie) {
-                    setcookie.forEach(
-                        function (cookiestr) {
-                            cookies.push(cookiestr);
-                        }
-                    );
-                }
-
-                //another chunk of data has been recieved, so append it to `str`
-                response.on('data', function (chunk) {
-                    str += chunk;
-                });
-
-                //the whole response has been recieved, so we just print it out here
-                response.on('end', function () {
-                    success(JSON.parse(str).claims_token.value);
-                });
-            };
-
-        http.request(options, callback).end();
-    },
-
-    getSessionConfirmationNumber = function (success) {
-        'use strict';
-
-        connectToMobileFabric(function (authToken) {
-            var options = {
-                host: host,
-                path: '/services/ATGRest/GetSessionConfirmationNumber',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Kony-Authorization': authToken,
-                    'Accept': 'application/json',
-                    'Cookie': cookies.join('; ')
-                }
-            },
-
-                callback = function (response) {
-                    var str = '', setcookie;
-
-                    setcookie = response.headers["set-cookie"];
-
-                    if (setcookie) {
-                        setcookie.forEach(
-                            function (cookiestr) {
-                                cookies.push(cookiestr);
-                            }
-                        );
-                    }
-
-                    //another chunk of data has been recieved, so append it to `str`
-                    response.on('data', function (chunk) {
-                        str += chunk;
-                    });
-
-                    //the whole response has been recieved, so we just print it out here
-                    response.on('end', function () {
-                        success(authToken);
-                    });
-                };
-
-            http.request(options, callback).end();
-        });
-    };
-
+/*global module*/
 module.exports.getProduct = function (req, res) {
     'use strict';
 
-    var productCode = req.params.codigo;
+    var product = {  
+        "contexto":{  
+            "sucesso":true
+        },
+        "codigoProduto":"535977248",
+        "nome":"Pijama Infantil com Estampa de Dinossauro - Tam 2 a 12 anos",
+        "tipoProduto":"APPAREL",
+        "cor":{  
+            "codigo":"535977248-5V",
+            "nome":"Marrom",
+            "fotosGrandes":[  
+                "//largeimg.lojasrenner.com.br/images/variant/large/535977248-5V_z_1.jpg"
+            ],
+            "fotosPequenas":[  
+                "/images/variant/icon/535977248-5V_v_1.jpg",
+                "//iconimg.lojasrenner.com.br/images/variant/icon/535977248-5V_v_2.jpg"
+            ],
+            "fotoPantone":"//colorimg.lojasrenner.com.br/images/variant/imagecolors/535977248-5V.jpg"
+        },
+        "tamanho":{  
+            "codigo":"6A",
+            "nome":"6A"
+        },
+        "sku":"535977281",
+        "categoriaEcommerce":[  
+            {  
+                "codigo":"cat180002",
+                "nome":"AllCategories",
+                "codigosPais":[  
 
-    getSessionConfirmationNumber(function (authToken) {
-        var postData = 'codigo=' + productCode,
-            options = {
-                host: host,
-                path: '/services/ATGRest/GetProduto',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/xml',
-                    'X-Kony-Authorization': authToken,
-                    'Cookie': cookies.join('; ')
-                }
+                ]
             },
-
-            callback = function (response) {
-                var str = '';
-
-                //another chunk of data has been recieved, so append it to `str`
-                response.on('data', function (chunk) {
-                    str += chunk;
-                });
-
-                //the whole response has been recieved, so we just print it out here
-                response.on('end', function () {
-                    res.send({ success: true, data: JSON.parse(str) });
-                });
-
-                response.on('error', function (error) {
-                    console.log(error);
-                });
+            {  
+                "codigo":"cat30004",
+                "nome":"Infantil",
+                "codigosPais":[  
+                    "cat180002"
+                ]
             },
+            {  
+                "codigo":"cat130005",
+                "nome":"Pijamas e Roupa Íntima",
+                "codigosPais":[  
+                    "cat30004"
+                ]
+            },
+            {  
+                "codigo":"cat1830679",
+                "nome":"Pijamas",
+                "codigosPais":[  
+                    "cat130005"
+                ]
+            }
+        ],
+        "categoriaRetail":{  
+            "grupo":{  
+                "codigo":"321",
+                "nome":"R_I_AcesInfantil"
+            },
+            "departamento":{  
+                "codigo":"4089",
+                "nome":"R_I_AcesInfantil_O"
+            },
+            "classe":{  
+                "codigo":"5238",
+                "nome":"Pijama"
+            },
+            "subclasse":{  
+                "codigo":"6368"
+            }
+        },
+        "cores":[  
+            {  
+                "codigo":"535977248-5V",
+                "nome":"Marrom",
+                "fotosGrandes":[  
+                    "//largeimg.lojasrenner.com.br/images/variant/large/535977248-5V_z_1.jpg"
+                ],
+                "fotosPequenas":[  
+                    "/images/variant/icon/535977248-5V_v_1.jpg",
+                    "//iconimg.lojasrenner.com.br/images/variant/icon/535977248-5V_v_2.jpg"
+                ],
+                "fotoPantone":"//colorimg.lojasrenner.com.br/images/variant/imagecolors/535977248-5V.jpg"
+            }
+        ],
+        "tamanhos":[  
+            {  
+                "codigo":"2A",
+                "nome":"2A"
+            },
+            {  
+                "codigo":"4A",
+                "nome":"4A"
+            },
+            {  
+                "codigo":"6A",
+                "nome":"6A"
+            },
+            {  
+                "codigo":"8A",
+                "nome":"8A"
+            },
+            {  
+                "codigo":"10A",
+                "nome":"10A"
+            },
+            {  
+                "codigo":"12A",
+                "nome":"12A"
+            }
+        ],
+        "skus":{  
+            "535977264":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"2A"
+            },
+            "535977272":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"4A"
+            },
+            "535977281":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"6A"
+            },
+            "535977299":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"8A"
+            },
+            "535977301":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"10A"
+            },
+            "535977310":{  
+                "codigoCor":"5V",
+                "codigoTamanho":"12A"
+            }
+        },
+        "manipulado":true
+    };
 
-            postRequest = http.request(options, callback);
+    product.codigoProduto = req.params.codigo;
 
-        postRequest.write(postData);
-        postRequest.end();
-    });
+    return res.json(product);
 };
 
 module.exports.getSKU = function (req, res) {
     'use strict';
 
-    var sku = req.params.sku;
-
-    getSessionConfirmationNumber(function (authToken) {
-        var postData = 'sku=' + sku,
-            options = {
-                host: host,
-                path: '/services/ATGRest/GetSku',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/xml',
-                    'X-Kony-Authorization': authToken,
-                    'Cookie': cookies.join('; ')
-                }
-            },
-
-            callback = function (response) {
-                var str = '';
-
-                //another chunk of data has been recieved, so append it to `str`
-                response.on('data', function (chunk) {
-                    str += chunk;
-                });
-
-                //the whole response has been recieved, so we just print it out here
-                response.on('end', function () {
-                    res.send({ success: true, data: JSON.parse(str) });
-                });
-            },
-
-            postRequest = http.request(options, callback);
-
-        postRequest.write(postData);
-
-        postRequest.end();
-    });
+    return res.json({ nome: 'falta obter um json mockado para sku.' });
 };
 
 module.exports.getPrice = function (req, res) {
